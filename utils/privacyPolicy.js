b206e9988127b537fe548844b58bc7937c692078
Junaid Iqbal <junaidiqbalkk@gmail.com>
2023-10-25 09:46:00 +0500

    commit-007

--- utils/privacyPolicy.js
+++ utils/privacyPolicy.js
@@ -1,52 +1,52 @@
-export const privacyPolicyContent = `Privacy Policy:
-
-Privacy Policy for TikTok Analytics App
-
-Last Updated: 25-Oct-2023
-
-1. Introduction
-
-Welcome to the Privacy Policy for the TikTok Analytics App. At TikTok Analytics App, we take your privacy seriously and are committed to protecting your personal information. This Privacy Policy outlines how we collect, use, share, and safeguard your data. By using our app, you consent to the practices described in this policy.
-
-2. Information We Collect
-
-We collect and process the following types of information:
-
-    Personal Information: This may include your name, email address, and any other information you provide to us.
-
-    Usage Data: We may collect data on how you interact with our app, such as the features you use and the content you access.
-
-    Device Information: We collect information about your device, including the device type, operating system, and unique identifiers.
-
-    Cookies and Tracking: We use cookies and similar tracking technologies to collect data about your usage of our app.
-
-3. How We Use Your Information
-
-We use your information for the following purposes:
-
-    To provide and maintain the app.
-    To improve our services and develop new features.
-    To communicate with you, respond to your inquiries, and send updates.
-    To personalize your experience and deliver relevant content.
-    To analyze and monitor usage patterns.
-    To comply with legal obligations.
-
-4. Data Sharing
-
-We do not sell, trade, or rent your personal information to third parties. However, we may share your data with trusted service providers and partners for the purposes outlined in this Privacy Policy.
-
-5. Data Security
-
-We implement security measures to protect your data from unauthorized access, disclosure, alteration, and destruction. However, no method of transmission over the internet or electronic storage is entirely secure, and we cannot guarantee absolute security.
-
-6. Your Rights
-
-You have the right to access, correct, or delete your personal information. You can also opt out of receiving communications from us. For more information about your rights, please contact us at pms.buggcy@gmail.com.
-
-7. Changes to This Policy
-
-We may update this Privacy Policy periodically to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will notify you of any significant changes through our app or via email.
-
-8. Contact Us
-
+export const privacyPolicyContent = `Privacy Policy:<br />
+<br />
+Privacy Policy for TikTok Analytics App<br />
+<br />
+Last Updated: 25-Oct-2023<br />
+<br />
+1. Introduction<br />
+<br />
+Welcome to the Privacy Policy for the TikTok Analytics App. At TikTok Analytics App, we take your privacy seriously and are committed to protecting your personal information. This Privacy Policy outlines how we collect, use, share, and safeguard your data. By using our app, you consent to the practices described in this policy.<br />
+<br />
+2. Information We Collect<br />
+<br />
+We collect and process the following types of information:<br />
+<br />
+    Personal Information: This may include your name, email address, and any other information you provide to us.<br />
+    <br />
+    Usage Data: We may collect data on how you interact with our app, such as the features you use and the content you access.<br />
+    <br />
+    Device Information: We collect information about your device, including the device type, operating system, and unique identifiers.<br />
+    <br />
+    Cookies and Tracking: We use cookies and similar tracking technologies to collect data about your usage of our app.<br />
+    <br />
+3. How We Use Your Information<br />
+<br />
+We use your information for the following purposes:<br />
+<br />
+    To provide and maintain the app.<br />
+    To improve our services and develop new features.<br />
+    To communicate with you, respond to your inquiries, and send updates.<br />
+    To personalize your experience and deliver relevant content.<br />
+    To analyze and monitor usage patterns.<br />
+    To comply with legal obligations.<br />
+    <br />
+4. Data Sharing<br />
+<br />
+We do not sell, trade, or rent your personal information to third parties. However, we may share your data with trusted service providers and partners for the purposes outlined in this Privacy Policy.<br />
+<br />
+5. Data Security<br />
+<br />
+We implement security measures to protect your data from unauthorized access, disclosure, alteration, and destruction. However, no method of transmission over the internet or electronic storage is entirely secure, and we cannot guarantee absolute security.<br />
+<br />
+6. Your Rights<br />
+<br />
+You have the right to access, correct, or delete your personal information. You can also opt out of receiving communications from us. For more information about your rights, please contact us at pms.buggcy@gmail.com.<br />
+<br />
+7. Changes to This Policy<br />
+<br />
+We may update this Privacy Policy periodically to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will notify you of any significant changes through our app or via email.<br />
+<br />
+8. Contact Us<br />
+<br />
 If you have any questions, concerns, or requests related to this Privacy Policy, please contact us at pms.buggcy@gmail.com.`;
--- utils/termsOfService.js
+++ utils/termsOfService.js
@@ -1,41 +1,41 @@
-export const termsOfServiceContent = `Terms of Service:
-
-Terms of Service for TikTok Analytics App
-
-Last Updated: 25-Oct-2023
-
-1. Acceptance of Terms
-
-By accessing or using the TikTok Analytics App ("the app"), you agree to be bound by these Terms of Service. If you do not agree to these terms, please do not use the app.
-
-2. Use of the App
-
-You may use the app for lawful purposes only. You are responsible for your actions and content shared through the app.
-
-3. Privacy Policy
-
-Your use of the app is also governed by our Privacy Policy. Please review it to understand our data collection and usage practices.
-
-4. Intellectual Property
-
-The content, features, and functionality of the app are protected by copyright, trademark, and other intellectual property laws. You may not reproduce, distribute, modify, or create derivative works without our prior written consent.
-
-5. Termination
-
-We reserve the right to terminate or suspend your access to the app at our discretion, without notice, for any reason, including violation of these Terms.
-
-6. Disclaimer
-
-The app is provided "as is" without warranties of any kind, either express or implied. We do not guarantee the accuracy, completeness, or reliability of the app's content.
-
-7. Limitation of Liability
-
+export const termsOfServiceContent = `Terms of Service:<br />
+<br />
+Terms of Service for TikTok Analytics App<br />
+<br />
+Last Updated: 25-Oct-2023<br />
+<br />
+1. Acceptance of Terms<br />
+<br />
+By accessing or using the TikTok Analytics App ("the app"), you agree to be bound by these Terms of Service. If you do not agree to these terms, please do not use the app.<br />
+<br />
+2. Use of the App<br />
+<br />
+You may use the app for lawful purposes only. You are responsible for your actions and content shared through the app.<br />
+<br />
+3. Privacy Policy<br />
+<br />
+Your use of the app is also governed by our Privacy Policy. Please review it to understand our data collection and usage practices.<br />
+<br />
+4. Intellectual Property<br />
+<br />
+The content, features, and functionality of the app are protected by copyright, trademark, and other intellectual property laws. You may not reproduce, distribute, modify, or create derivative works without our prior written consent.<br />
+<br />
+5. Termination<br />
+<br />
+We reserve the right to terminate or suspend your access to the app at our discretion, without notice, for any reason, including violation of these Terms.<br />
+<br />
+6. Disclaimer<br />
+<br />
+The app is provided "as is" without warranties of any kind, either express or implied. We do not guarantee the accuracy, completeness, or reliability of the app's content.<br />
+<br />
+7. Limitation of Liability<br />
+<br />
 We shall not be liable for any indirect, incidental, special, consequential, or punitive damages arising out of your use of the app.
-
-8. Changes to the Terms
-
+<br />
+8. Changes to the Terms<br />
+<br />
 We may update these Terms of Service from time to time. Your continued use of the app after changes are posted constitutes your acceptance of the modified terms.
-
-9. Contact Us
-
+<br />
+9. Contact Us<br />
+<br />
 If you have any questions or concerns about these Terms of Service, please contact us at pms.buggcy@gmail.com.`;
